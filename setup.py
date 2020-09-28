61331cd86bb120bf79f155cf1ebf81336b4a3145
g-kienzle <kienzle2003@gmail.com>
2020-09-27 20:45:37 -0400

    Modified setup

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import setuptools
 
 setuptools.setup(
     name="studentvue_cli",
-    version="0.1",
+    version="0.2",
     description="A useful module",
     packages=["studentvue_cli"],  # same as name
     entry_points={"console_scripts": ["svue = studentvue_cli.studentvue_cli:main"]},
     license="MIT",
     url="https://github.com/g-kienzle/studentvue_cli",
-    download_url="https://github.com/g-kienzle/studentvue_cli/archive/v0.1.tar.gz",
+    download_url="https://github.com/g-kienzle/studentvue_cli/archive/v0.2.tar.gz",
     package_data={"": ["config.ini"]},
     include_package_data=True,
     install_requires=[
